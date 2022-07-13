8f5a31eeb5303de40d38dd40623154b7a550477d
KN-tiger <108563821+KN-tiger@users.noreply.github.com>
2022-07-13 05:04:09 +0000

    [Finish]4-15

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  
+  def show
+    
+    @user = User.find(params[:id])
+    @post_images = @user.post_images
+    
+  end
+
+  def edit
+    
+    
+    
+  end
+  
+  private
+
+  def user_params
+    
+    params.require(:user).permit(:shop_name, :image, :caption)
+    
+  end
+  
+end
