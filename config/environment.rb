e56fb44f0317ef3b1770dea828e6e35cb8e67e48
KN-tiger <108563821+KN-tiger@users.noreply.github.com>
2022-07-15 06:12:12 +0000

    [Finish]4-24

--- config/environment.rb
+++ config/environment.rb
@@ -5,12 +5,3 @@ require_relative "application"
 Rails.application.initialize!
 
 
-const webpack = require('webpack')
-environment.plugins.prepend(
-  'Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery',
-    Popper: 'popper.js'
-  })
-)
